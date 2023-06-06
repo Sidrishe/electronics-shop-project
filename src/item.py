24c624e2b8c1341a8c8812dc743d656ca21bc925
Mikhail <osipov.m93@mail.ru>
2023-06-06 20:10:30 +0300

    fix func

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+﻿import os
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """Вернет название товара"""
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        """Проверяем длину названия товара"""
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            raise ValueError("Длина наименования товара не более 10 симвовов")
+            # print("Длинна наименования товара не должна превышать 10 симвовов")
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +49,22 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Инициализируюем экземпляр класса `Item` данными из файла _src/items.csv"""
+        cls.all.clear()
+        op_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src', "items.csv")
+        with open(op_file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name, price, quantity = row["name"], cls.string_to_number(row["price"]), int(row["quantity"])
+                cls(name, price, quantity)
+            #     name = row["name"]
+            #     price = cls.string_to_number(row["price"])
+            #     quantity = int(row["quantity"])
+
+    @staticmethod
+    def string_to_number(value):
+        """Преобразование строки в число"""
+        return int(float(value))
